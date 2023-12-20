107a879087a384e002abf055720f615e64d6f3e0
Lily Barrett <11barrettpl@gmail.com>
2023-12-20 19:26:04 +1300

    Added pages to main branch

---
+++ FrontEnd/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styling/Header.css'
+
+function Header() {
+    return (
+        <div className="headerContainer">
+            <h1 className="headings textContent">RentShare</h1>
+    </div>
+)
+}
+
+export default Header
