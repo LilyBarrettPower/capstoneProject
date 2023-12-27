05e9e317115ff1386b08759238ca28f640b7213a
Lily Barrett <11barrettpl@gmail.com>
2023-12-27 17:02:55 +1300

    user registration working

--- BackEnd/Routes/usersRoutes.js
+++ BackEnd/Routes/usersRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const Controllers = require('../Controllers')
 
+// General CRUD operations:
 router.get('/', (req, res) => {
     Controllers.userController.getUser(res);
 });
 
+// Can delete this route as have the register route! :) 
 router.post('/create', (req, res) => {
     Controllers.userController.createUser(req.body, res);
 })
@@ -17,6 +19,11 @@ router.put('/:UserID', (req, res) => {
 
 router.delete('/:UserID', (req, res) => {
     Controllers.userController.deleteUser(req, res);
+})
+
+// user registration route:
+router.post('/register', (req, res) => {
+    Controllers.userController.registerUser(req, res);
 })
 
 module.exports = router;
