6a8bee1b771fbeb3ed7d684abb75432a1f219b12
Lily Barrett <11barrettpl@gmail.com>
2023-12-31 08:41:51 +1300

    itemCard to DB

--- FrontEnd/src/pages/ProfilePage.jsx
+++ FrontEnd/src/pages/ProfilePage.jsx
@@ -20,10 +20,6 @@ function ProfilePage() {
 
     console.log(currentUser);
     // Testing item data before adding the database
-    const itemsData = [
-        { ItemName: 'test item 1', ItemDescription: 'testing', ItemPricePerDay: 20, ItemFeaturedImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIF_8BvUeewoE1lJ7Y16ibY250r7R0_0XfLZtg3Vj6cA&s', ItemCategory: 'animals', ItemLocation: 'Canterbury', ItemSecondImage: 'https://img.freepik.com/free-photo/isolated-happy-smiling-dog-white-background-portrait-4_1562-693.jpg', ItemThirdImage: 'https://images.pexels.com/photos/1490908/pexels-photo-1490908.jpeg?cs=srgb&dl=pexels-svetozar-milashevich-1490908.jpg&fm=jpg' },
-        { ItemName: 'test item 2', ItemDescription: 'testing', ItemPricePerDay: 40, ItemFeaturedImage: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*', ItemCategory: 'animals', ItemLocation: 'Canterbury', ItemSecondImage: 'https://img.freepik.com/free-photo/isolated-happy-smiling-dog-white-background-portrait-4_1562-693.jpg', ItemThirdImage: 'https://images.pexels.com/photos/1490908/pexels-photo-1490908.jpeg?cs=srgb&dl=pexels-svetozar-milashevich-1490908.jpg&fm=jpg' }
-    ];
 
     return (
         <>
@@ -38,12 +34,8 @@ function ProfilePage() {
                     <Col md={10}> {/* Adjust the column size based on your needs */}
                         <h3 className='headings'>For rent:</h3>
                         <CreatePostButton /> 
-                        {/* Need to change this so that it comes up beside the For rent heading */}
-                    <div className='d-flex flex-column justify-content-around'>
-                        {itemsData.map((item, index) => (
-                            <ItemCard key={index} itemData={item} />
-                        ))}
-                    </div>
+                        {/* HERE WILL BE THE RENTED AND SAVED ITEMS RENDERED HERE!  */}
+    
                 </Col>
             </Row>
             </Container>
--- FrontEnd/src/pages/TimelinePage.jsx
+++ FrontEnd/src/pages/TimelinePage.jsx
@@ -2,19 +2,72 @@
 import NavBar from "../components/NavBar";
 import Header from '../components/Header';
 import LogOutButton from "../components/LogOutButton";
+import ItemCard from "../components/ItemCard";
+import { useState, useEffect } from 'react';
 
+// import the relevant bootstrap:
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/esm/Container';
+
+import { useUserContext } from '../context/userContext';
 
 function TimelinePage() {
+    // Use the context custom hook
+    const { currentUser } = useUserContext();
+    const [itemsData, setItemsData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true); // Add loading state
+
+    // Fetch data when the component mounts
+    useEffect(() => {
+        const fetchItems = async () => {
+            try {
+                const response = await fetch('http://localhost:3307/rentshare/items/getall');
+                if (response.ok) {
+                    const result = await response.json();
+                    setItemsData(result.data);
+                } else {
+                    console.error('Error fetching items', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error fetching items', error);
+            } finally {
+                setIsLoading(false); // Set loading state to false regardless of success or failure
+            }
+        };
+
+        fetchItems();
+    }, []); // Empty dependency array ensures that the effect runs once, similar to componentDidMount
+
+    // Render the component
+    if (isLoading) {
+        return <p>Loading...</p>; // Render a loading indicator while data is being fetched
+    }
 
     return (
         <>
             <Header></Header>
             <NavBar></NavBar>
             <LogOutButton></LogOutButton>
-            <h1> Timeline page</h1>
-
+            <h1>Timeline page</h1>
+            <Container fluid style={{ paddingLeft: '80px', paddingRight: '20px' }}>
+                <Row>
+                    <Col md={10}>
+                        {itemsData.length > 0 ? (
+                            <div className='d-flex flex-column justify-content-around'>
+                                {itemsData.map((item, index) => (
+                                    <ItemCard key={index} itemData={item} />
+                                ))}
+                            </div>
+                        ) : (
+                            <p>No items available.</p>
+                        )}
+                    </Col>
+                </Row>
+            </Container>
         </>
-    )
+    );
 }
 
 export default TimelinePage;
+
