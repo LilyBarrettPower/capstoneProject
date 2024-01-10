c96fcbc1eff5c3d7e7477dd06cea96b6d0e301a3
Lily Barrett <11barrettpl@gmail.com>
2024-01-10 18:11:55 +1300

    updated some UI

--- FrontEnd/src/components/ProfileInfo.jsx
+++ FrontEnd/src/components/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/esm/Container';
 import { useEffect } from 'react';
+import CreatePostButton from './CreatePostButton';
 
 import { useUserContext } from '../context/userContext';
 
@@ -26,8 +27,8 @@ function ProfileInfo() {
     }, [currentUser]);
 
     return (
-        <Container className="d-flex flex-column align-items-center" style={{width: '10%', margin: '10px'}}>
-            <Image src={ProfilePhoto} alt={FullName} roundedCircle style={{ width: '150px', height: '150px', objectFit: 'cover', marginBottom: '10px' }} />
+        <Container className="d-flex flex-column align-items-center" style={{ width: '10%', margin: '10px' }}>
+            <Image src={ProfilePhoto} alt={FullName} roundedCircle style={{ width: '150px', height: '150px', objectFit: 'cover', marginBottom: '10px', border: '2px solid #000' }} />
             <div>
                 <p className="body">{FullName}</p>
                 <p className="body">{Location}</p>
