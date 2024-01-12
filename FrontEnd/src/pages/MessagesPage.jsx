8e42ef227363fe259f3717cab30673d7157ad5e4
Lily Barrett <11barrettpl@gmail.com>
2024-01-13 11:34:49 +1300

    comments

--- FrontEnd/src/pages/MessagesPage.jsx
+++ FrontEnd/src/pages/MessagesPage.jsx
@@ -3,7 +3,7 @@ import NavBar from "../components/NavBar";
 import Header from '../components/Header';
 import LogOutButton from "../components/LogOutButton";
 import Messages from "../components/Messages";
-// import SearchMessages from "../components/SearchMessages";
+
 
 
 function MessagesPage() {
@@ -15,8 +15,6 @@ function MessagesPage() {
             <NavBar></NavBar>
             <LogOutButton></LogOutButton>
             <Messages></Messages>
-            {/* <SearchMessages></SearchMessages> */}
-
         </>
     )
 }
